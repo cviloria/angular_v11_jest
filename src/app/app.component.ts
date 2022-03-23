d06ebcd4084078b9630f47b46ea85d67a372d47c
Carlos Viloria <cviloria@bancolombia.com.co>
2022-03-23 15:29:02 -0500

    feat: eslint with husky config fenix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { BcHeaderNavItem } from "bancolombia-design-system/bc-header";
 
 @Component({
 	selector: "app-root",
@@ -7,4 +8,25 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
 	title = "codigoAsesor";
+	orientation = "";
+	selected = "valorPrueba1";
+	companyList: BcHeaderNavItem[] = [
+		{ icon: "store", text: "Company 1", subText: "Nit: 1111111-1" },
+		{ icon: "store", text: "Company 2", subText: "Nit: 2222222-2" },
+		{ icon: "store", text: "Company 3", subText: "Nit: 3333333-3" }
+	];
+
+	logout() {
+		console.log("salir");
+	}
+
+	changeCompany(res: CustomEvent) {
+		alert("company: " + JSON.stringify(res.detail));
+	}
+	onChangeTab(ev){}
+
+	showSelected(event: any) {
+		console.log(event)
+		this.selected = event;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,36 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
+import { BcFooterModule } from "bancolombia-design-system/bc-footer";
+
+import { BcIconModule } from "bancolombia-design-system/bc-icon";
+import { BcHeaderModule } from "bancolombia-design-system/bc-header";
+import { BcButtonModule } from "bancolombia-design-system/bc-button";
+import { BcInputModule } from "bancolombia-design-system/bc-input";
+import { BcBtnGroupModule } from "bancolombia-design-system/bc-button-group";
+import { BcTabsModule } from "bancolombia-design-system/bc-tabs-group";
+import { BcLoaderModule } from "bancolombia-design-system/bc-loader";
+import { BcNotificationModule } from "bancolombia-design-system/bc-notification";
+
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule],
+	imports: [
+		BrowserModule,
+		BcFooterModule,
+		BcIconModule.forRoot({
+			path: "https://library-sdb.apps.bancolombia.com/bds/2.1.3"
+		}),
+		BcHeaderModule,
+		BcButtonModule,
+		BcInputModule,
+		BcButtonModule,
+		BcBtnGroupModule,
+		BcTabsModule,
+		BcLoaderModule,
+		BcNotificationModule
+
+	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
